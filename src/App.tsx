7868d9e6b8404e3a076c9de32f95631b7d221739
mattb20 <matt_burgoyne@live.co.uk>
2023-10-26 13:49:56 +0100

    add MainGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,14 @@ function Header() {
   );
 }
 
+function MainGoal() {
+  return ( 
+      <div>
+       My main goal: learning the fundamentals of React!
+      </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -29,9 +37,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+       <MainGoal/>
       </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
