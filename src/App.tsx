7fa3c02f028f7aeb738a5c507f440680263774fd
mattb20 <matt_burgoyne@live.co.uk>
2023-11-04 14:58:16 +0000

    introduce select handler to tab  buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css';
 import TabButton from './components/TabButton.tsx';
 
 function App() {
+  function handleSelect() {
+    console.log('Clicked on');
+  }
   return (
     <>
       <Header title='Vite + React'></Header>
@@ -15,10 +18,10 @@ function App() {
       <section id="examples">
         <h2>Examples</h2>
         <menu>
-          <TabButton>Components</TabButton>
-          <TabButton>JSX</TabButton>
-          <TabButton>Props</TabButton>
-          <TabButton>State</TabButton>
+          <TabButton onSelect={handleSelect}>Components</TabButton>
+          <TabButton onSelect={handleSelect}>JSX</TabButton>
+          <TabButton onSelect={handleSelect}>Props</TabButton>
+          <TabButton onSelect={handleSelect}>State</TabButton>
         </menu>
       </section>
     </>
--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,3 +1,3 @@
-export default function TabButton({ children }): React.JSX.Element {
-  return <button>{children}</button>
-} 
+export default function TabButton({ children, onSelect }): React.JSX.Element {
+  return <button onClick={onSelect}>{children}</button>;
+}
