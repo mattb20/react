07bcd94aba68dee5b06be5e040b020bca676cd7e
mattb20 <matt_burgoyne@live.co.uk>
2023-11-06 16:55:22 +0000

    toggle button css based on which section is selected

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,27 @@ type Section = 'components' | 'jsx' | 'props' | 'state';
 
 const sectionDescriptions = {
   components: {
-    details: 'Components are really useful for compartmentalising your front end code and encouraging reusability.'
+    details:
+      'Components are really useful for compartmentalising your front end code and encouraging reusability.',
   },
   jsx: {
-    details: 'JSX is an extended language of JavaScript which enables you to inbed JavaScript attributes and functions into HTML code to create dynamic front ends.'
+    details:
+      'JSX is an extended language of JavaScript which enables you to inbed JavaScript attributes and functions into HTML code to create dynamic front ends.',
   },
   props: {
-    details: 'Props can be used as part of the customisable process of creating components.'
+    details:
+      'Props can be used as part of the customisable process of creating components.',
   },
   state: {
-    details: 'State is the internal snapshot of your component at any time, including all the functions that React defines on the functional component.'
+    details:
+      'State is the internal snapshot of your component at any time, including all the functions that React defines on the functional component.',
   },
 };
 
 function App() {
   const [section, setSection] = useState('');
   function handleSelect(section: Section) {
-    setSection(sectionDescriptions[section].details);
+    setSection(section);
   }
   return (
     <>
@@ -38,14 +42,27 @@ function App() {
         <h2>Examples</h2>
         <menu>
           <TabButton
+            isSelected={section === 'components'}
             onSelect={() => handleSelect('components')}
             title={'Components'}
           />
-          <TabButton onSelect={() => handleSelect('jsx')} title={'JSX'} />
-          <TabButton onSelect={() => handleSelect('props')} title={'Props'} />
-          <TabButton onSelect={() => handleSelect('state')} title={'State'} />
+          <TabButton
+            isSelected={section === 'jsx'}
+            onSelect={() => handleSelect('jsx')}
+            title={'JSX'}
+          />
+          <TabButton
+            isSelected={section === 'props'}
+            onSelect={() => handleSelect('props')}
+            title={'Props'}
+          />
+          <TabButton
+            isSelected={section === 'state'}
+            onSelect={() => handleSelect('state')}
+            title={'State'}
+          />
         </menu>
-        {!section ? 'Please select a topic': <div>{section}</div>}
+        {!section ? 'Please select a topic' : <div>{sectionDescriptions[section].details}</div>}
       </section>
     </>
   );
