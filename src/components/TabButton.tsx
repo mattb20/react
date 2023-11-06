e62e397c880d2d083095815e8da3126996e69ecb
mattb20 <matt_burgoyne@live.co.uk>
2023-11-06 16:12:51 +0000

    update index.css

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,3 +1,3 @@
 export default function TabButton({ title, onSelect }): React.JSX.Element {
-  return <button onClick={onSelect}>{title}</button>;
+  return <button className="active" onClick={onSelect}>{title}</button>;
 }
