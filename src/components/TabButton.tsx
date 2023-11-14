3c29504c5c31e8b561064246bddf58112cc2906d
mattb20 <matt_burgoyne@live.co.uk>
2023-11-14 16:03:43 +0000

    add types to TabButton component

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,3 +1,7 @@
-export default function TabButton({ title, onSelect, isSelected }): React.JSX.Element {
+export default function TabButton({ title, onSelect, isSelected }: {
+  title: string;
+  onSelect: any;
+  isSelected: boolean;
+}): React.JSX.Element {
   return (<button className={isSelected ? 'active' : undefined} onClick={onSelect}>{title}</button>);
 }
